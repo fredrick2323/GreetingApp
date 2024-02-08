eb3c282773330c653dfde01123d3710277fecf28
dorong.276539 <dorong.276539@LAB-A-PC30>
2024-02-08 08:34:18 +0800

    Add project files.

---
+++ GreetingApp/GreetingProgram.cs
@@ -0,0 +1,30 @@
+﻿namespace GreetingApp
+{
+    class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+
+            Console.Write("Enter the number of enrolled courses: ");
+            int numCourses = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Enter the price of your favorite book: ");
+            double bookPrice = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Number of enrolled courses: " + numCourses);
+            Console.WriteLine("Price of favorite book: $" + bookPrice);
+
+
+            Console.WriteLine("\nPress Any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
